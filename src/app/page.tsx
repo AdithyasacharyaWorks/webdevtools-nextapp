c3406195df798f3cbcdb40a4858ce959068a9182
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-07-17 01:27:42 +0530

    removed auto caching from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Card } from "@/components/Card";
-import axios from "axios";
 import LandingPage from "@/components/LandingPage";
 
 export default async function Home() {
-
-  const tools = await axios.get('https://webdevtools-version1.netlify.app/api/tools')
+  const response = await fetch('https://webdevtools-version1.netlify.app/api/tools', { cache: 'no-store' });
+  const tools = await response.json();
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
       <div className="flex justify-center">
         <SignedIn>
-          <Card tools={tools?.data?.data}/>
+          <Card tools={tools?.data?.data} />
         </SignedIn>
         <SignedOut>
           <LandingPage />
